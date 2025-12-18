abcfa6758b25bb71d6aab7a327d4f0cd4cb1eae2
M4LPhoenix <l25120318@morelia.tecnm.mx>
2025-12-18 13:15:22 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+package com.tecnm.morelia.itics.poo.archivos.bags;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        // Crear manejadores de archivos
+        ManejoArchivosTexto archivosTexto = new ManejoArchivosTexto();
+        ManejoArchivosBinarios archivosBinarios = new ManejoArchivosBinarios();
+
+        // Crear y capturar una inversión
+        Inversion inversion = new Inversion();
+        inversion.capturarDatos();
+        inversion.mostrarInformacion();
+
+        // Guardar datos en archivos
+        archivosTexto.escribir("inversion.txt", inversion.toString());
+        archivosBinarios.escribir("inversion.dat", inversion);
+
+        System.out.println("\n✅ Datos guardados en inversion.txt e inversion.dat\n");
+
+        // Leer desde los archivos
+        System.out.println("📄 Contenido del archivo de texto:");
+        System.out.println(archivosTexto.leer("inversion.txt"));
+
+        System.out.println("\n💾 Objeto leído desde el archivo binario:");
+        Inversion leida = (Inversion) archivosBinarios.leer("inversion.dat");
+        leida.mostrarInformacion();
     }
 }
