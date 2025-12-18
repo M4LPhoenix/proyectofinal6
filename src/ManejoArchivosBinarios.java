cac2ebcbe1288df62cfbd53b664aee2c9962f419
M4LPhoenix <l25120318@morelia.tecnm.mx>
2025-12-18 13:15:50 -0600

    Update ManejoArchivosBinarios.java

--- src/ManejoArchivosBinarios.java
+++ src/ManejoArchivosBinarios.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+package com.tecnm.morelia.itics.poo.archivos.bags;
+
+import java.io.*;
 
 public class ManejoArchivosBinarios {
+
+    public void escribir(String nombreArchivo, Object objeto) throws IOException {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(nombreArchivo))) {
+            oos.writeObject(objeto);
+        }
+    }
+
+    public Object leer(String nombreArchivo) throws IOException, ClassNotFoundException {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(nombreArchivo))) {
+            return ois.readObject();
+        }
+    }
 }
