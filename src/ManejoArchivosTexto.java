78d8a29b739bf85d31d452bdfbf450d2e521a6fb
M4LPhoenix <l25120318@morelia.tecnm.mx>
2025-12-18 13:16:06 -0600

    Update ManejoArchivosTexto.java

--- src/ManejoArchivosTexto.java
+++ src/ManejoArchivosTexto.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+package com.tecnm.morelia.itics.poo.archivos.bags;
+
+import java.io.*;
 
 public class ManejoArchivosTexto {
+
+    public void escribir(String nombreArchivo, String contenido) throws IOException {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(nombreArchivo))) {
+            bw.write(contenido);
+        }
+    }
+
+    public String leer(String nombreArchivo) throws IOException {
+        StringBuilder contenido = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader(nombreArchivo))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                contenido.append(linea).append("\n");
+            }
+        }
+        return contenido.toString();
+    }
 }
